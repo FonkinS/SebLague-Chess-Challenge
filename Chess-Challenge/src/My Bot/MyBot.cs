44e11a9a7c0f3626499e89fb7bcfeefcfbbfe463
FonkinS <85748962+FonkinS@users.noreply.github.com>
2023-07-21 16:57:36 +0200

    just testing that gits working

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,6 @@
 ﻿using ChessChallenge.API;
 
+// This super simple sample bot by SebLague simply returns the first possible move it can think of
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
